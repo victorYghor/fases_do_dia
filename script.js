255ff8728868ea98c6bd7a36e37dc7ccc67c470e
Victor Yghor Simões dos Santos <124019555+vitilygo@users.noreply.github.com>
2023-02-21 14:15:31 -0300

    versão final

---
+++ script.js
@@ -0,0 +1,34 @@
+function carregar(){
+
+var msg = document.querySelector("div#msg")
+var img = document.getElementById('imagem')
+var data = new Date()
+// var hora = data.getHours()
+hora = 3
+var titulo = document.getElementById('h1')
+titulo.innerText =`${hora} horas`
+
+if (hora > 6 && hora <12) {
+    msg.innerHTML += "\n Bom dia!"
+    img.src = "manha2.png"
+    document.body.style.backgroundColor ='#f7eccd'
+    document.body.style.color = 'black'
+
+} else if (hora > 12 && hora <= 18) {
+    msg.innerHTML += ("\n Boa tarde!")
+    img.src = 'tarde1.png'
+    document.body.style.backgroundColor = '#fcad08'
+    
+} else if (hora >= 18 && hora < 24 ) {
+    msg.innerText += ("\n Boa noite!")
+    img.src = 'noite1.png'
+    document.body.style.backgroundColor ='#022a3d'
+} else {
+    msg.innerText = ("Boa madrugada!")
+    img.src = 'madrugada1.png'
+    document.body.style.backgroundColor = '#818998'
+    
+}    
+}
+ 
+
